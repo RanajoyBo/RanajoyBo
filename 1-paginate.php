8088dfa470477c3a368012d917ff625645289d3d
RanajoyBo <157569402+RanajoyBo@users.noreply.github.com>
2024-01-24 11:57:54 +0530

    Add files via upload

---
+++ 1-paginate.php
@@ -0,0 +1,29 @@
+
+</div>
+<form method="get" action="1-paginate.php">
+  <input type="hidden" name="page" value="1">
+  <input type="text" name="search" id="search">
+  <input type="submit" value="Go!">
+</form>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="get" action="1-paginate.php">
+  <input type="hidden" name="page" value="1">
+  <input type="text" name="search" id="search">
+  <input type="submit" value="Go!">
+</form>
+
+
+ 
+</div>
+</body>
+</html>
